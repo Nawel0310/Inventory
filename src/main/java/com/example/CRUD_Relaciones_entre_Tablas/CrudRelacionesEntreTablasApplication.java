c1ac5237cc324b7e77c6374062acf9898786715d
Nawel <nbarbaro@frba.utn.edu.ar>
2024-04-05 16:19:20 -0300

    Commit inicial

---
+++ src/main/java/com/example/CRUD_Relaciones_entre_Tablas/CrudRelacionesEntreTablasApplication.java
@@ -0,0 +1,13 @@
+package com.example.CRUD_Relaciones_entre_Tablas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudRelacionesEntreTablasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudRelacionesEntreTablasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/CRUD_Relaciones_entre_Tablas/CrudRelacionesEntreTablasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.CRUD_Relaciones_entre_Tablas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudRelacionesEntreTablasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
