317b737727ead3169c18cba9091af91752a02ccb
Nawel <nbarbaro@frba.utn.edu.ar>
2024-04-05 16:43:21 -0300

    Se creo la entidad Categoria

---
+++ src/main/java/com/example/CRUD_Relaciones_entre_Tablas/entities/Categoria.java
@@ -0,0 +1,23 @@
+package com.example.CRUD_Relaciones_entre_Tablas.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.Accessors;
+
+@Entity
+@Data
+@AllArgsConstructor
+@Accessors(chain = true)
+@NoArgsConstructor
+public class Categoria {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    @Column(length=45, nullable = false, unique=true)
+    private String nombre;
+
+
+
+}
